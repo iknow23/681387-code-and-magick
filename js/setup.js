b95abd2ab2246d45f46f4b50e582d637c70f7327
Aynur <felix-xilef@mail.ru>
2018-11-21 22:41:46 +0300

    Устранил замечания Тревиса

--- js/setup.js
+++ js/setup.js
@@ -1,18 +1,20 @@
+'use strict'
+
 var PLAYER_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var PLAYER_LAST_NAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var PLAYER_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var PLAYER_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-//создаю функцию для поиска случайного числа в пределах max,min значений
+//  создаю функцию для поиска случайного числа в пределах max,min значений
 function randomInteger(min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1)
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 }
 
-//вместо индекса в массиве вставляю генератор случайного числа:
-//для name - от 0 до 7
-//для coatColor - от 0 до 5
-//для eyesColor - от 0 до 4
+//  вместо индекса в массиве вставляю генератор случайного числа:
+//  для name - от 0 до 7
+//  для coatColor - от 0 до 5
+//  для eyesColor - от 0 до 4
 var players = [
   {
     name: PLAYER_NAMES[randomInteger(0, 7)] + PLAYER_LAST_NAMES[randomInteger(0, 7)],
@@ -34,13 +36,13 @@ var players = [
     coatColor: PLAYER_COAT_COLORS[randomInteger(0, 5)],
     eyesColor: PLAYER_EYES_COLORS[randomInteger(0, 4)]
   }
-]
+];
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-//вставляю игроков в соответствии с шаблоном
-var renderWizard = function(wizard) {
+//  вставляю игроков в соответствии с шаблоном
+var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -48,7 +50,7 @@ var renderWizard = function(wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < players.length; i++) {
@@ -56,7 +58,7 @@ for (var i = 0; i < players.length; i++) {
 }
 similarListElement.appendChild(fragment);
 
-//показываю окно настроек игрока
+//  показываю окно настроек игрока
 var setupModal = document.querySelector('.setup');
 setupModal.classList.remove('hidden');
 
--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -7,15 +9,14 @@ var GAP_X = 40;
 var GAP_Y = 40;
 var FONT_GAP = 15;
 var BAR_WIDTH = 40;
-var barHeight = CLOUD_HEIGHT/2 - 5;
-var BAR_HEIGHT = CLOUD_HEIGHT - FONT_GAP - GAP_Y - GAP_Y;
+var barHeight = CLOUD_HEIGHT / 2 - 5;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -27,7 +28,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
@@ -40,15 +41,15 @@ window.renderStatistics = function(ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + GAP_X + BAR_WIDTH*i + GAP_X*i, CLOUD_HEIGHT - GAP);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_X + BAR_WIDTH*i + GAP_X*i, CLOUD_HEIGHT - GAP + (-(barHeight*times[i]) / maxTime) - GAP_Y);
+    ctx.fillText(names[i], CLOUD_X + GAP_X + BAR_WIDTH * i + GAP_X * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_X + BAR_WIDTH * i + GAP_X * i, CLOUD_HEIGHT - GAP + (-(barHeight * times[i]) / maxTime) - GAP_Y);
     ctx.fillStyle = (i === 0) ? 'rgba(255, 0, 0, 1)' : 'rgba(2, 14, 134, 1)';
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
     else {
-      ctx.fillStyle = 'rgba(2, 14, 134, '+ Math.random() +')';
+      ctx.fillStyle = 'rgba(2, 14, 134, ' + Math.random() + ')';
     }
-    ctx.fillRect(CLOUD_X + GAP_X + BAR_WIDTH*i + GAP_X*i, CLOUD_Y*23, BAR_WIDTH, -(barHeight*times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + GAP_X + BAR_WIDTH * i + GAP_X * i, CLOUD_Y * 23, BAR_WIDTH, -(barHeight * times[i]) / maxTime);
   }
 };
