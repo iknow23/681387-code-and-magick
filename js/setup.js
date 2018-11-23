82fe2b66f77b97812752aacf22aa235c7adddd35
Aynur <felix-xilef@mail.ru>
2018-11-23 11:10:21 +0300

    Переписал функцию, создающую рандомных игроков

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,8 @@ var PLAYER_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var PLAYER_LAST_NAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var PLAYER_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var PLAYER_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_QUANTITY = 4;
+
 //  создаю функцию для поиска случайного числа в пределах max,min значений
 function getRandomInteger(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -11,45 +13,23 @@ function getRandomInteger(min, max) {
   return rand;
 }
 
-// //создаю нулевой массив игроков
-// var players = [];
-//
-// //функция для создания 1 игрока
-// //цикл --внутри--> функция создающая одного игрока + добавление в массив
-// var createPlayer = function () {
-//   //создаю 1 объект
-//   var player = {
-//     name: PLAYER_NAMES[getRandomInteger(0, 7)] + PLAYER_LAST_NAMES[getRandomInteger(0, 7)],
-//     coatColor: PLAYER_COAT_COLORS[getRandomInteger(0, 5)],
-//     eyesColor: PLAYER_EYES_COLORS[getRandomInteger(0, 4)]
-//   };
-//   return player;
-//   //пушаю 1 объект в массив
-//   players.push("player");
-// }
-//
-// console.log(createPlayer());
-//
-// //функция для создания 4 игроков
-// var createPlayers = function (playersQuantity) {
-//   for (var i = 1; i <= playersQuantity; i++) {
-//     createPlayer();
-//   }
-// };
-//
-// //создаю массив объектов с игроками
-// players = [createPlayers(4)];
-
+//начальный (пустой) массив игроков
 var players = [];
-
-for (var i = 0; i < 4; i++) {
-  var player = {
-    name: PLAYER_NAMES[getRandomInteger(0, 7)] + PLAYER_LAST_NAMES[getRandomInteger(0, 7)],
-    coatColor: PLAYER_COAT_COLORS[getRandomInteger(0, 5)],
-    eyesColor: PLAYER_EYES_COLORS[getRandomInteger(0, 4)]
+ //создаю игроков
+//цикл --внутри--> функция создающая одного игрока + добавление в массив
+var createPlayers = function (playersQuantity) {
+  for (var i = 1; i <= playersQuantity; i++) {
+    //создаю 1 объект
+    var player_i = {
+      name: PLAYER_NAMES[getRandomInteger(0, 7)] + PLAYER_LAST_NAMES[getRandomInteger(0, 7)],
+      coatColor: PLAYER_COAT_COLORS[getRandomInteger(0, 5)],
+      eyesColor: PLAYER_EYES_COLORS[getRandomInteger(0, 4)]
+    }
+    players.push(player_i);
   };
-  players.push(player);
-}
+};
+ //создаю массив объектов с игроками
+createPlayers(WIZARDS_QUANTITY);
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
